42576666a745345fc4ec94913d0ca83de2b10405
RaviTeja2206 <103409351+RaviTeja2206@users.noreply.github.com>
2023-02-17 17:23:50 +0530

    Version 0.1

--- EmployeeHierarchy.py
+++ EmployeeHierarchy.py
@@ -35,6 +35,8 @@ SPACE_PREFIX = "    "
 
 dep=0
 h=''
+EH=[]
+cnt=0
 for x in result:
 
     if x.get('level')==0:
@@ -49,10 +51,13 @@ for x in result:
                 h=h[:len(SPACE_PREFIX)*i]+PIPE_PREFIX+h[len(SPACE_PREFIX)*i+1:]
         
         h=h.replace(TEE,ELBOW)
-        print(h)
+        #print(h)
+        EH.append(h)
         h=f"{SPACE_PREFIX*x.get('level')}{TEE}{x.get('employee_name')}"
         
+        
         dep=x.get('level')
+        cnt=0
     elif x.get('level')<dep:
         if dep>=2:
           
@@ -61,7 +66,17 @@ for x in result:
                 
                 h=h[:len(SPACE_PREFIX)*i]+PIPE_PREFIX+h[len(SPACE_PREFIX)*i+1:]
         h=h.replace(TEE,ELBOW)
-        print(h)
+        #print(h)
+
+        if abs(dep-x.get('level'))!=1:
+            for u in range(cnt):
+                cs=EH[len(EH)-u-1]
+                for m in range(dep-x.get('level')-1):
+                    cs=cs[:len(SPACE_PREFIX)*(dep-x.get('level')-m)]+" "+cs[len(SPACE_PREFIX)*(dep-x.get('level')-m)+1:]
+                EH[len(EH)-u-1]=cs
+            
+        cnt=0
+        EH.append(h)
         h=f"{SPACE_PREFIX*x.get('level')}{TEE}{x.get('employee_name')}"
         dep=x.get('level')
     elif x.get('level')==dep:
@@ -70,14 +85,22 @@ for x in result:
             for i in range(1,dep):
                 
                 h=h[:len(SPACE_PREFIX)*i]+PIPE_PREFIX+h[len(SPACE_PREFIX)*i+1:]
-        print(h)
+        #print(h)
+        EH.append(h)
         h=f"{SPACE_PREFIX*x.get('level')}{TEE}{x.get('employee_name')}"
         dep=x.get('level')
+        cnt+=1
         
     else:
         pass
 h=h.replace(TEE,ELBOW)
-print(h)
+#print(h)
+EH.append(h)
+
+#print('='*30)
+
+for k in EH:
+    print(k)
     
 
 
